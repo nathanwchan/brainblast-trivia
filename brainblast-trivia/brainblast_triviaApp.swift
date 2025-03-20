372100e7422c8a2014e0a859584c221b8df1ab82
Nate Chan <nchan87@gmail.com>
2025-03-20 11:00:15 -0700

    Initial Commit

---
+++ brainblast-trivia/brainblast_triviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainblast_triviaApp.swift
+//  brainblast-trivia
+//
+//  Created by Nathan Chan on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct brainblast_triviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
